ad45e7696b62a2834072c62fad6ade22eb4bc786
Antonio Cucchiara (TA) <antonio.cucchiara@hmcts.net>
2017-01-19 13:11:02 +0000

    Avoid double-loading of the root node.

--- src/main/java/org/publo/controller/MainViewController.java
+++ src/main/java/org/publo/controller/MainViewController.java
@@ -100,6 +100,5 @@ public final class MainViewController implements Initializable {
         menubarPaneController.init(source, pageTemplate, file);
         fileBrowser.addSelectionListener(new ResourceChangedListener<>(source));
         fileBrowser.addSelectionListener(new ResourceChangedListener<>(file));
-        fileBrowser.setRoot(Launcher.PROJECTS_PATH);
     }
 }
